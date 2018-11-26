68a2be493877a330e01ce9dc431e31bd529fc96e
Travis Decker <decker4693@gmail.com>
2018-11-26 08:37:42 -0700

    Disabled menu option until future use.

--- app/src/main/java/com/shaker/shaker/controller/MainActivity.java
+++ app/src/main/java/com/shaker/shaker/controller/MainActivity.java
@@ -290,11 +290,11 @@ public class MainActivity extends AppCompatActivity
 
   @Override
   public boolean onOptionsItemSelected(MenuItem item) {
-
-    int id = item.getItemId();
-    if (id == R.id.action_settings) {
-      return true;
-    }
+//
+//    int id = item.getItemId();
+//    if (id == R.id.action_settings) {
+//      return true;
+//    }
     return super.onOptionsItemSelected(item);
   }
 
